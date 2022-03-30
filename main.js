d5b2ff3e29206d0bcd7b67f040a326ad352a8f19
Babatunde99 <adeboyebabatunde93@gmail.com>
2022-03-30 15:27:28 +0100

    update made

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function myfunction(){
     bum2.classList.remove('change')
     bum3.classList.remove('change')
     bum4.classList.remove('change')
-    
+    localStorage.setItem('rank','1')
 }
 function mytake(){
     bum1.classList.add("change")
@@ -12,6 +12,7 @@ function mytake(){
     bum2.classList.remove('change')
     bum3.classList.remove('change')
     bum4.classList.remove('change')
+    localStorage.setItem('rank','2')
 }
 function myfeel(){
     bum2.classList.add("change")
@@ -19,12 +20,15 @@ function myfeel(){
     bum1.classList.remove('change')
     bum3.classList.remove('change')
     bum4.classList.remove('change')
+    localStorage.setItem('rank','3')
 }
 function myyarn(){
     bum3.classList.add("change")
     bum1.classList.remove('change')
     bum2.classList.remove('change')
     bum4.classList.remove('change')
+    localStorage.setItem('rank','4')
+
 }
 function myjob(){
     bum4.classList.add("change")
@@ -32,4 +36,9 @@ function myjob(){
     bum2.classList.remove('change')
     bum3.classList.remove('change')
     bum1.classList.remove('change')
+    localStorage.setItem('rank','5')
+
 }
+
+ var x = localStorage.getItem('rank')
+ show.innerHTML = x 
