8b6e728a1d9796029bb181e0cb96e71b097ccfe2
Babatunde99 <adeboyebabatunde93@gmail.com>
2022-03-30 12:02:48 +0100

    progress made

---
+++ main.js
@@ -0,0 +1,35 @@
+function myfunction(){
+    bum.classList.add('change');
+    bum1.classList.remove('change')
+    bum2.classList.remove('change')
+    bum3.classList.remove('change')
+    bum4.classList.remove('change')
+    
+}
+function mytake(){
+    bum1.classList.add("change")
+    bum.classList.remove('change')
+    bum2.classList.remove('change')
+    bum3.classList.remove('change')
+    bum4.classList.remove('change')
+}
+function myfeel(){
+    bum2.classList.add("change")
+    bum.classList.remove('change')
+    bum1.classList.remove('change')
+    bum3.classList.remove('change')
+    bum4.classList.remove('change')
+}
+function myyarn(){
+    bum3.classList.add("change")
+    bum1.classList.remove('change')
+    bum2.classList.remove('change')
+    bum4.classList.remove('change')
+}
+function myjob(){
+    bum4.classList.add("change")
+    bum.classList.remove('change')
+    bum2.classList.remove('change')
+    bum3.classList.remove('change')
+    bum1.classList.remove('change')
+}
